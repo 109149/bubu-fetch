5bee34fb1823a5fed00fa7eaa8febe900189862b
109149 <109149qwe@gmail.com>
2021-03-02 21:52:03 +0400

    refactor: update hierarchy

---
+++ src/components/app/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import * as dogCeo from "../../api/dog-ceo";
+// import { v4 as uuid } from "uuid";
+import "./App.css";
+
+// TODO: DevTools
+function App() {
+  return <h1>Bubu Fetch</h1>;
+}
+
+export default App;
